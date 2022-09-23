648178616f5a0d940c7ab62fbcbb7df49975c610
Tyler Guerricabeitia <hunets4506@gmail.com>
2022-09-23 08:47:31 -0600

    working add button and css changes

---
+++ script.js
@@ -0,0 +1,7 @@
+let hiddenList = document.getElementById('createListName');
+let addButton = document.getElementById('addList');
+
+function showListInput() {
+    let showList = hiddenList.style.display="inline";
+    let hideAddButton = addButton.style.display="none";
+}
